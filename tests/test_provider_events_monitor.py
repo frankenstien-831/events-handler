d1fa6b56731adcbbb0ca9183baf820c71e6ab193
ssallam <samer@oceanprotocol.com>
2019-08-26 11:02:05 +0200

    Fix test and copy artifacts before starting test.

--- tests/test_provider_events_monitor.py
+++ tests/test_provider_events_monitor.py
@@ -14,7 +14,7 @@ from tests.resources.mocks.aquarius import AquariusMock
 def test_init_events_monitor(keeper, web3, storage_path, provider_account):
     events_monitor = ProviderEventsMonitor(keeper, web3, storage_path, provider_account)
     assert events_monitor.last_n_blocks == events_monitor.LAST_N_BLOCKS
-    assert events_monitor.latest_block == Web3Provider.get_web3().eth.blockNumber
+    assert (Web3Provider.get_web3().eth.blockNumber - events_monitor.latest_block) < 5
     assert events_monitor.last_processed_block == 0
 
 
@@ -52,26 +52,35 @@ def test_process_pending_agreements(keeper, web3, storage_path, provider_account
         agr_2: {'accessSecretStore': 1, 'lockReward': 1, 'escrowReward': 1},
         agr_3: {'accessSecretStore': 2, 'lockReward': 2, 'escrowReward': 1}
     }
+    balance = keeper.token.get_token_balance(consumer.address)/(2**18)
+    if balance < 20:
+        keeper.dispenser.request_tokens(100, consumer)
+
     lock_reward(agr_1, sa, consumer)
     lock_reward(agr_3, sa, consumer)
     grant_access(agr_3, ddo, consumer, provider_account)
     event = keeper.access_secret_store_condition.subscribe_condition_fulfilled(
-        agr_3, 25, None, (), wait=True
+        agr_3, 35, None, (), wait=True
     )
-    assert event, f'grant access failed for agreement {agr_3}'
+    if not event:
+        # check status
+        cond_to_status = get_conditions_status(agr_3)
+        print(f'agr_3 condition status: {cond_to_status}')
+        if cond_to_status['accessSecretStore'] != 2:
+            raise AssertionError(f'grant access failed for agreement {agr_3}')
 
     AquariusProvider.set_aquarius_class(AquariusMock)
     events_monitor = ProviderEventsMonitor(keeper, web3, storage_path, provider_account)
     events_monitor.process_pending_agreements(pending_agreements, conditions)
 
     keeper.access_secret_store_condition.subscribe_condition_fulfilled(
-        agr_1, 10, None, (), wait=True
+        agr_1, 15, None, (), wait=True
     )
     keeper.escrow_reward_condition.subscribe_condition_fulfilled(
-        agr_1, 10, None, (), wait=True
+        agr_1, 15, None, (), wait=True
     )
     keeper.escrow_reward_condition.subscribe_condition_fulfilled(
-        agr_3, 10, None, (), wait=True
+        agr_3, 15, None, (), wait=True
     )
 
     # check status of all agreements
@@ -84,7 +93,10 @@ def test_process_pending_agreements(keeper, web3, storage_path, provider_account
     events_monitor.start_agreement_events_monitor()
     lock_reward(agr_2, sa, consumer)
     keeper.access_secret_store_condition.subscribe_condition_fulfilled(
-        agr_2, 10, None, (), wait=True
+        agr_2, 15, None, (), wait=True
+    )
+    keeper.escrow_reward_condition.subscribe_condition_fulfilled(
+        agr_2, 15, None, (), wait=True
     )
     cond_to_status = get_conditions_status(agr_2)
     assert [2, 2, 2] == list(cond_to_status.values()), \
