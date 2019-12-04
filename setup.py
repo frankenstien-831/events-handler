6d565e0c44c06e66983ea8017c7eb4c9df6f0e56
ssallam <samer@oceanprotocol.com>
2019-12-04 13:00:12 +0100

    Bump version: 0.3.1 → 0.3.2

--- setup.py
+++ setup.py
@@ -86,6 +86,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/oceanprotocol/events-handler-py',
-    version='0.3.1',
+    version='0.3.2',
     zip_safe=False,
 )
