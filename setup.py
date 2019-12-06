0b64ebf792687dd2893458da1e82b0a040465975
ssallam <samer@oceanprotocol.com>
2019-12-06 14:25:35 +0100

    Revert to older version of ocean-keeper lib.

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ with open('CHANGELOG.md') as history_file:
 # or pip install -e .
 install_requirements = [
     'ocean-utils==0.3.2',
-    'ocean-keeper==0.3.1',
+    'ocean-keeper==0.3.0',
     'ocean-secret-store-client==0.0.2',
     'PyYAML==4.2b4',
 ]
